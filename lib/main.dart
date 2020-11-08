a48a691924f7ca896b9297f53ce682fef516d3e0
João Prates <jovictor833@gmail.com>
2020-11-08 17:16:39 -0300

    Test checkbox itens

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:entrada_dados/CampoTexto.dart';
+import 'package:entrada_dados/EntradaCheckbox.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(
       MaterialApp(
-        home: CampoTexto(),
+        //home: CampoTexto(),
+        home: EntradaCheckbox(),
       )
   );
 }
